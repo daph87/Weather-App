0858677acee0ff29d0c01d2b36010ddebcaebe71
daph <87daphnelevy@gmail.com>
2021-10-29 08:12:37 +0300

    dep

--- src/global.ts
+++ src/global.ts
@@ -1,3 +1,4 @@
 /** @format */
 
-export const apikey = "HJEPQTZCWxhq8IqpXFNwM9vbUgHi1PHP";
+// export const apikey = "HJEPQTZCWxhq8IqpXFNwM9vbUgHi1PHP";
+export const apikey = "CmA4vAXD306u0pdSGGwlchO5lC5VSjFs";
